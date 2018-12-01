3d5d4bc59623a987531df1a023cd570a0de6b75c
simon <simondebn@gmail.com>
2018-12-01 17:54:08 +0100

    Advent of Code - 2018 - Day 1

---
+++ Day 1/aoc_d1.py
@@ -0,0 +1,28 @@
+# ADVENT OF CODE 2018 - DAY 1 #
+input_file_path = './inputs_aoc_d1.txt'
+
+
+# Part 1 #
+def aoc_freq(filepath):
+    freq = 0
+    with open(filepath) as fp:
+        for cnt, line in enumerate(fp):
+            freq += int(line)  # if line[0] == '+' else freq - int(line[1:])
+    return freq
+
+
+# Part 2 #
+def aoc_freq_redundant(filepath):
+    seen = {0}
+    freq = 0
+    while True:
+        with open(filepath) as fp:
+            for cnt, line in enumerate(fp):
+                freq += int(line)
+                if freq in seen:
+                    return freq
+                seen.add(freq)
+
+
+print(aoc_freq(input_file_path))
+print(aoc_freq_redundant(input_file_path))
